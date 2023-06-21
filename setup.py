b532c3ca1a49ef4457c1a868b3cef694e185f8a6
Edward Evans <elevans2@wisc.edu>
2023-06-21 09:58:18 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="overpixel",
+    version="0.1.0.dev0",
+    author="Edward Evans",
+    author_email="elevans2@wisc.edu",
+    platforms=["any"],
+    description="A package for analyzing two images for pixel correlation.",
+)
