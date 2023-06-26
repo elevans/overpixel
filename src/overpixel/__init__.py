1294d3c20525acedd5ceb19101150e86603919c6
Edward Evans <elevans2@wisc.edu>
2023-06-26 09:57:07 -0500

    Add load data helper module

--- src/overpixel/__init__.py
+++ src/overpixel/__init__.py
@@ -8,14 +8,13 @@ def parse_args():
     parser.add_argument('--input', '-i', required=True, type=str, help="Path to input .tif file.")
     parser.add_argument('--method', '-m', choices=['binary_overlap', 'saca'], required=True, help="Method to analyze the dataset.")
     parser.add_argument('--show', '-s', action='store_true', help='Show results.')
-    parser.add_argument('--batch', '-b', action='store_true', help='Set the input as a batch job.')
 
     return parser.parse_args()
 
 def main():
     args = parse_args()
     if args.method == 'binary_overlap':
-        results = overlap.compute(util.load_file(args.input), show=args.show)
+        results = overlap.run(util.load_data(args.input), show=args.show)
         print(f"[DEBUG]: {results}")
 
 if __name__ == "__main__":
--- src/overpixel/util.py
+++ src/overpixel/util.py
@@ -1,6 +1,10 @@
-import numpy as np
+from typing import List
 from tifffile import imread
 
-def load_file(path: str) -> np.ndarray:
+def load_data(path: str):
+    """
+    Load input data from the path.
+    """
+    # TODO: Add logic to check the shape of the input array.
     narr = imread(path)
     return narr.transpose(1, 2, 0)
